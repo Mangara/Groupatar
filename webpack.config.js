375dcf4456216a71051495624144e1ca3ac6c0bf
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2019-02-16 13:21:04 -0500

    More config setup

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,16 @@ module.exports = {
     path: __dirname+'/dist',
     filename: 'app.bundle.js'
   },
+  devtool: "source-map", // Enable sourcemaps for debugging webpack's output.
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
   module: {
     rules: [
-      { test: /\.(ts|tsx)$/, loader: 'awesome-typescript-loader' }
+      { test: /\.(ts|tsx)$/, loader: 'awesome-typescript-loader' },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   }
 };
