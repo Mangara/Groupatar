6896bff4b8cfaa02e32b75b77a03a7aff1fe3ad5
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2019-03-03 20:46:13 -0500

    Use new layout

--- app/components/AvatarGroup.tsx
+++ app/components/AvatarGroup.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import * as Gravatar from 'gravatar';
 
-import hexGridLayout from '../utils/hexGridLayout';
+import {Placement} from '../utils/Layout';
+import GridLayout from '../utils/GridLayout';
 
 const WIDTH = 1600;
 const HEIGHT = 900;
-const MARGIN = 5;
-const SPACING_FACTOR = 2 / Math.sqrt(3);
 
 export interface Props {
     emails: string[];
@@ -27,23 +26,11 @@ export default class AvatarGroup extends React.Component<Props, {}> {
         const ctx = this.canvas && this.canvas.getContext('2d');
         
         if (ctx) {
-            ctx.clearRect(0,0, WIDTH, HEIGHT);
-            
             const {emails} = this.props;
-            const layout = hexGridLayout(emails.length);
-            const nRows = layout.length;
-            const size = computeSize(layout);
+            const layout = new GridLayout().layout(emails.length, WIDTH, HEIGHT);
             
-            const xOffset = (WIDTH - width(layout) * size) / 2 - left(layout) * size;
-            const yOffset = (HEIGHT - nRows * size) / 2;
-            
-            let emailIdx = 0;
-            layout.forEach((row, rowIdx) => {
-                const x = xOffset + (rowIdx % 2 === 1 ? size * SPACING_FACTOR / 2 : 0);
-                const avatarCount = row.filter(v => v).length;
-                drawRow(row, emails.slice(emailIdx, emailIdx + avatarCount), x, yOffset + rowIdx * size, ctx, size);
-                emailIdx += avatarCount;
-            });
+            ctx.clearRect(0,0, WIDTH, HEIGHT);
+            drawLayout(ctx, emails, layout);
         }
     }
     
@@ -59,15 +46,11 @@ export default class AvatarGroup extends React.Component<Props, {}> {
     }
 }
 
-function drawRow(row: boolean[], emails: string[], x: number, y: number, ctx: CanvasRenderingContext2D, size: number) {
-    const fullWidth = size * SPACING_FACTOR;
-    let emailIdx = 0;
-    row.forEach((val, idx) => {
-        if (val) {
-            drawAvatar(emails[emailIdx], ctx, x + fullWidth * idx, y, size);
-            emailIdx++;
-        }
-    });
+function drawLayout(ctx: CanvasRenderingContext2D, emails: string[], layout: Placement) {
+    const {coords, size} = layout;
+    for (let i = 0; i < emails.length; i++) {
+        drawAvatar(emails[i], ctx, coords[i].x - size/2, coords[i].y - size/2, size);
+    }
 }
 
 function drawAvatar(email: string, ctx: CanvasRenderingContext2D, x: number, y: number, size: number) {
@@ -89,45 +72,4 @@ function drawAvatar(email: string, ctx: CanvasRenderingContext2D, x: number, y: 
     };
 
     img.src = Gravatar.url(email, {s: String(Math.round(size))}, true);
-}
-
-function computeSize(layout: boolean[][]) {
-    return Math.min(horizontalSize(layout), verticalSize(layout.length));
-}
-
-function horizontalSize(layout: boolean[][]) {
-    return (WIDTH - 2 * MARGIN) / width(layout);
-}
-
-function verticalSize(rows: number) {
-    return (HEIGHT - 2 * MARGIN) / rows;
-}
-
-function width(layout: boolean[][]) {
-    let left = Infinity;
-    let right = 0;
-    
-    layout.forEach((row, idx) => {
-        const offset = idx % 2 === 1 ? SPACING_FACTOR / 2 : 0;
-        const first = row.indexOf(true) * SPACING_FACTOR;
-        const last = row.lastIndexOf(true) * SPACING_FACTOR + 1;
-        
-        left = Math.min(left, offset + first);
-        right = Math.max(right, offset + last);
-        console.log(`row: ${JSON.stringify(row)} left: ${offset + first} right: ${offset + last}`);
-    });
-    console.log(`left: ${left} right: ${right}`);
-    return right - left;
-}
-
-function left(layout: boolean[][]) {
-    let left = Infinity;
-    
-    layout.forEach((row, idx) => {
-        const offset = idx % 2 === 1 ? SPACING_FACTOR / 2 : 0;
-        const first = row.indexOf(true) * SPACING_FACTOR;
-        
-        left = Math.min(left, offset + first);
-    });
-    return left;
 }
