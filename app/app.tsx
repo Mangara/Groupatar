cd5c4c8213d1dcabc7f447d42702272dd9eea5e5
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2019-03-27 22:22:41 -0400

    Delete everything

--- app/app.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import Groupatar from './Groupatar';
-
-ReactDOM.render(
-  <Groupatar />,
-  document.getElementById('app')
-);
